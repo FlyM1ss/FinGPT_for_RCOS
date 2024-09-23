7e664c8e255a18d974b305817774145f5d899939
Oskar Lee <oskar@nordlee.com>
2024-09-23 12:26:27 -0400

    Install Dependencies
    Shell script to install dependencies

---
+++ installDependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if ! command -v pip &> /dev/null; then
+    echo "pip could not be found. Installing pip..."
+    sudo apt update
+    sudo apt install -y python3-pip
+fi
+pip install python-dotenv
+pip install django
+pip install django-cors-headers
+pip install bs4
+pip install django-request
+pip install openai==0.28
+pip install google
+pip install transformers
+pip install torch
+pip install accelerate
+pip install tensorflow
