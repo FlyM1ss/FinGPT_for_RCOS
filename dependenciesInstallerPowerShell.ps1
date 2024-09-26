7f74cd0c41f67bd63cced809f98807df834af0dd
Oskar Lee <oskar@nordlee.com>
2024-09-25 21:27:04 -0400

    installer update

---
+++ dependenciesInstallerPowerShell.ps1
@@ -0,0 +1,11 @@
+pip install python-dotenv
+pip install django
+pip install django-cors-headers
+pip install bs4
+pip install django-request
+pip install openai==0.28
+pip install google
+pip install transformers
+pip install torch
+pip install accelerate
+pip install tensorflow
