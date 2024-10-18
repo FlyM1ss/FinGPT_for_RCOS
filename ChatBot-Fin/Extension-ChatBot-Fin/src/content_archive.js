dab9f8f658df7d20129a5731104f1a48e5115c5c
Chris Sandoval <christian@sandoval.ph>
2024-10-18 17:03:40 -0400

    Update content_archive.js

--- ChatBot-Fin/Extension-ChatBot-Fin/src/content_archive.js
+++ ChatBot-Fin/Extension-ChatBot-Fin/src/content_archive.js
@@ -152,14 +152,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const advAskButton = document.getElementById('advAskButton');
     const textbox = document.getElementById('textbox');
 
-<<<<<<< Updated upstream
-
-=======
-    if (!textbox) {
-        console.error('Textbox element not found');
-        return;
-    }
->>>>>>> Stashed changes
     console.log('Textbox element found');
 
     askButton.addEventListener('click', get_chat_response);
