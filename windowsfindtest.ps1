ec1778c59dcb3216fddaef0c5c872215bcf0646d
Oskar Lee <oskar@nordlee.com>
2024-10-30 16:42:38 -0400

    update

--- windowsfindtest.ps1
+++ windowsfindtest.ps1
@@ -1,5 +1,22 @@
 $curDir=$PSScriptRoot
-$extensionPath= Join-Path $curDir "ChatBot-Fin/Extension-ChatBot-Fin/src"
+$extensionPath= Join-Path $curDir "ChatBot-Fin\Extension-ChatBot-Fin\src"
+
+$paths = @(
+    "C:\Program Files\Google\Chrome\Application",
+    "C:\Program Files (x86)\Google\Chrome\Application"
+)
+
+$foundPaths = @()
+
+$ChromePath
+
+foreach ($path in $paths) {
+    if (Test-Path $path) {
+        $ChromePath = $path
+    }
+}
 
 #load extension
 Write-Host $extensionPath
+Start-Process $ChromePath "chrome.exe" -ArgumentList "--load-extension=$extensionPath"
+Write-Host "FinGPT extension loaded onto Chrome"
